d826939e3f94aebdee8cc8325d78f026b424eae5
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-08 18:07:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,6 +13,27 @@ class Transfer
   end 
   
   def execute_transaction
-    if(@status)
-  end 
+     
+    if(@status == "pending")
+      sender.balance -= amount
+      if(valid?)
+        receiver.balance += amount
+        @status = "complete"
+        @status.freeze
+      else
+        @status = "rejected"
+        @status.freeze 
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+  end
+  
+  def reverse_transfer
+    if(status == "complete")
+      sender.balance += amount 
+      receiver.balance -= amount
+      @status = "reversed" 
+      @status.freeze
+    end
+  end
 end
