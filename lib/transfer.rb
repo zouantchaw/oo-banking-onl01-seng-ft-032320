a33b9c7e4cf1824f9f5f38e3c8b46f4a12836413
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-08 17:47:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
+  attr_accessor :sender, :reciever, :amount
   
+  def initialize(sender, reciever, amount)
+    @sender = sender 
+    @reciever = reciever
+    @amount = amount
+    @status = "pending"
+  end 
 end
